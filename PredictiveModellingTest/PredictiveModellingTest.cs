8c595f173d731e99de1dcbc5637eae5c6872efb2
saboyle <boylesa@gmail.com>
2019-04-07 01:15:30 +0100

    Working XUnit tests with Cake file in place.

--- PredictiveModellingTest/PredictiveModellingTest.cs
+++ PredictiveModellingTest/PredictiveModellingTest.cs
@@ -1,20 +1,16 @@
-using NUnit.Framework;
+using Xunit;
 using PredictiveModelling;
 
 namespace PrectiveModellingTest
 {
-    [TestFixture]
-    public class ProbabilitySpace2dTesOverallShape
+    public class ShapeWithP1Supremacy
     {
-        private ProbabilitySpace2d ps1;
+        private readonly ProbabilitySpace2d ps1;
+        private readonly double pL;
+        private readonly double pU;
+        private readonly double pD;
 
-        [SetUp]
-        public void Setup()
-        {
-        }
-
-        [Test]
-        public void TestCorrectDominanceWithExp1Supremacy()
+        public ShapeWithP1Supremacy()
         {
             this.ps1 = new ProbabilitySpace2d(3.1, 2.1, 13);
 
@@ -22,37 +18,41 @@ namespace PrectiveModellingTest
             double pU = ps1.AggregateUpper();
             double pD = ps1.AggregateDiagonal();
 
-            Assert.Greater(pL, pU);
-            Assert.Greater(pL, pD);
-            Assert.Greater(pU, pD);
+            this.pL = this.ps1.AggregateLower();
+            this.pU = this.ps1.AggregateUpper();
+            this.pD = this.ps1.AggregateDiagonal();
         }
 
-        [Test]
-        public void TestCorrectDominanceWithExp2Supremacy()
+        [Fact]
+        public void TestCorrectDominanceWithP1Supremacy()
         {
-            this.ps1 = new ProbabilitySpace2d(2.1, 3.1, 13);
-
-            double pL = ps1.AggregateLower();
-            double pU = ps1.AggregateUpper();
-            double pD = ps1.AggregateDiagonal();
-
-            Assert.Greater(pU, pL);
-            Assert.Greater(pL, pD);
-            Assert.Greater(pU, pD);
+            Assert.True(this.pL > this.pU);
+            Assert.True(this.pL > this.pD);
+            Assert.True(this.pU > this.pD);
         }
+    }
 
-        [Test]
-        public void TestCorrectDominanceWithNoSupremacy()
+    public class ShapeWithP2Supremacy
+    {
+        private readonly ProbabilitySpace2d ps1;
+        private readonly double pL;
+        private readonly double pU;
+        private readonly double pD;
+
+        public ShapeWithP2Supremacy()
         {
             this.ps1 = new ProbabilitySpace2d(2.1, 2.1, 13);
 
-            double pL = ps1.AggregateLower();
-            double pU = ps1.AggregateUpper();
-            double pD = ps1.AggregateDiagonal();
+            this.pL = this.ps1.AggregateLower();
+            this.pU = this.ps1.AggregateUpper();
+            this.pD = this.ps1.AggregateDiagonal();
+        }
 
-            Assert.AreEqual(pU, pL);
-            Assert.Greater(pU, pD);
-            Assert.Greater(pL, pD);
+        public void TestCorrectDominanceWithP2Supremacy()
+        {
+            Assert.True(this.pL < this.pU);
+            Assert.True(this.pL > this.pD);
+            Assert.True(this.pU > this.pD);
         }
     }
 }
